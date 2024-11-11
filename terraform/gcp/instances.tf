b9a51699f8e1d889af3a8943fb5c5a439a5d6797
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2024-11-11 10:56:29 +0200

    test-pr

--- terraform/gcp/instances.tf
+++ terraform/gcp/instances.tf
@@ -26,3 +26,7 @@ resource google_compute_instance "server" {
 resource google_compute_disk "unencrypted_disk" {
   name = "terragoat-${var.environment}-disk"
 }
+
+resource google_compute_disk "unencrypted_disk2" {
+  name = "terragoat-${var.environment}-disk"
+}
