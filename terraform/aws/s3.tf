0921f8052cf79c29a1aba023a2c09fc86de0f239
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2025-03-30 11:56:25 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -12,6 +12,9 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
+resource "aws_s3_bucket_object" "blanc" {
+}
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
@@ -48,6 +51,7 @@ resource "aws_s3_bucket" "operations" {
   tags = {
     Name        = "${local.resource_prefix.value}-operations"
     Environment = local.resource_prefix.value
+    tagtag = "tag"
   }
 
 }
